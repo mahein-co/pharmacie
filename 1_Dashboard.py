e16b0d741a3aa76cf303aa01a76421f74aaef65e
BenitoFezay <benitofezay@gmail.com>
2025-07-23 10:31:27 +0300

    Application de requetes pour medicaments.

--- 1_Dashboard.py
+++ 1_Dashboard.py
@@ -5,11 +5,13 @@ import plotly.express as px
 import duckdb
 from utils import load_data
 from db import init_duckdb
-# from sklearn.linear_model import LinearRegression
-# from sklearn.model_selection import train_test_split
-import numpy as np
+# import numpy as np
+# import asyncio
 
 from data.mongodb_ip_manager import MongoDBIPManager
+from data.mongodb_client import MongoDBClient
+from data import mongodb_pipelines
+
 
 # Initialisation
 st.set_page_config(page_title="Dashboard Pharmacie", layout="wide")
@@ -41,11 +43,46 @@ with st.sidebar:
     st.sidebar.image("images/logoMahein.png", caption="", use_container_width=True)
 
 
-if df is not None and "medicament" in df and "stock" in df:
+
+# Initialisation a MongoDB
+vente_collection = MongoDBClient(collection_name="vente")
+medicament_collection = MongoDBClient(collection_name="medicament")
+employe_collection = MongoDBClient(collection_name="employe")
+
+# I- FIRST LINE OF SCORECARD
+if vente_collection and medicament_collection:
+    # 1. chiffre d'affaire total
+    chiffre_affaire = vente_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_chiffre_affaire, title="Calcul du chiffre d'affaire")
+    try:
+        total_chiffre_affaire = chiffre_affaire[0]["montant_total"] if chiffre_affaire else 0
+    except Exception as e:
+        st.error(f"❌ Erreur lors du calcul du chiffre d'affaire : {e}")
+        total_chiffre_affaire = 0
+
+    # # 2. valeur totale du stock
+    # valeur_stock = vente_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_somme_valeur_stock, title="Calcul de la valeur totale du stock")
+    # try:
+    #     valeur_totale_stock = valeur_stock[0]["valeur_totale_stock"] if valeur_stock else 0
+    # except Exception as e:
+    #     st.error(f"❌ Erreur lors du calcul de la valeur totale du stock : {e}")
+    #     valeur_totale_stock = 0
+        
+    # 3. nombre total de vente
+    nombre_total_vente = vente_collection.count_distinct_agg(field_name="id_vente")
+
+    # 4. nombre total d'alimentation
+    nombre_alimentation = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_nombre_alimentations, title="Recuperation de nombre total d'alimentation")
+    try:
+        nombre_total_alimentation = nombre_alimentation[0]["nombre_total_alimentations"] if nombre_alimentation else 0
+    except Exception as e :
+        st.error(f"❌ Erreur lors du calcul du nombre total d'alimentation : {e}")
+        nombre_total_alimentation = 0
+
+
     # Extraction des DataFrames
     medicament_df = df["medicament"]
     stock_df = df["stock"]
-
+    
     # Fusion des données
     merged_df = pd.merge(stock_df, medicament_df, on="ID_Medicament", how="left")
 
@@ -55,24 +92,26 @@ if df is not None and "medicament" in df and "stock" in df:
 
     # 💊 Titre principal
     st.markdown("""
-        <h1 style='font-size: 32px; color: #4CAF50; margin-bottom: 0;'>PHARMACIE METROPOLE</h1>
+        <h1 style='font-size: 32px; color: #4CAF50; margin-bottom: 0;'>PHARMACIE MÉTROPOLE</h1>
         <p style='font-size: 16px; color: gray;'>Vue d'ensemble des indicateurs clés</p>
         <hr style='margin-top: 10px; margin-bottom: 20px;' />
     """, unsafe_allow_html=True)
 
     # 🔎 Indicateurs SQL
     metrics_queries = {
-        "💰 Chiffre d'affaires total": "SELECT SUM(Prix_Vente * Stock_Disponible) FROM pharmacie",
-        "📦 Valeur totale du stock": "SELECT SUM(Stock_Disponible) FROM pharmacie",
-        "🔢 Nombre total de ventes": "SELECT COUNT(DISTINCT Nom_Commercial) FROM pharmacie",
-        "⚠️Nombre total d’alimentation": "SELECT COUNT(*) FROM pharmacie WHERE Stock_Disponible < 10"
-    }
+        "Chiffre d'affaires total": f"{total_chiffre_affaire:,}".replace(",", " ") + "&nbsp; MGA",
+        # "📦 Valeur totale du stock": f"{valeur_totale_stock:,}".replace(",", " ") + " MGA",
+        "🔢 Nombre total de ventes": f"{nombre_total_vente:,}".replace(",", " "),
+        "⚠️Nombre total d’alimentation": nombre_total_alimentation
+        # "⚠️Nombre total d’alimentation": "SELECT COUNT(*) FROM pharmacie WHERE Stock_Disponible < 10"
+        # "📦 Valeur totale du stock": "SELECT SUM(Stock_Disponible) FROM pharmacie",
+    }   
 
     st.markdown("""
         <style>
             .metric-box {
                 border-left: 5px solid #4CAF50;
-                padding: 10px 15px;
+                padding: 10px 7px;
                 margin-bottom: 15px;
                 border-radius: 6px;
                 box-shadow: 1px 1px 4px rgba(0,0,0,0.05);
@@ -94,15 +133,15 @@ if df is not None and "medicament" in df and "stock" in df:
 
     try:
         # Répartir les métriques en colonnes
-        cols = st.columns(4)
-        for i, (label, query) in enumerate(metrics_queries.items()):
-            value = con.execute(query).fetchone()[0]
-            value = "N/A" if value is None else f"{value:,.2f}"
+        cols = st.columns(3)
+        for i, (key, value) in enumerate(metrics_queries.items()):
+            # value = con.execute(query).fetchone()[0]
+            # value = "N/A" if value is None else f"{value:,.2f}"
 
             # Affichage HTML perfsonnalisé avec bordure gauche
             html_metric = f"""
                 <div class="metric-box">
-                    <div class="metric-label">{label}</div>
+                    <div class="metric-label">{key}</div>
                     <div class="metric-value">{value}</div>
                 </div>
             """
@@ -110,10 +149,8 @@ if df is not None and "medicament" in df and "stock" in df:
 
     except Exception as e:
         st.error(f"❌ Une erreur est survenue lors du calcul des indicateurs : {e}")
-
 else:
-    st.error("❌ Les données 'medicament' et 'stock' sont manquantes ou invalides.")
-
+    st.error("Il est impossible de charger les données depuis la database.")
 
 
 # Appliquer des styles CSS personnalisés pour les métriques
@@ -140,7 +177,29 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-if df is not None and "medicament" in df and "stock" in df and "detailVente" in df:
+# II- SECOND LINE OF SCORECARD
+if medicament_collection and employe_collection:
+    # 2.1. Nombre total de médicaments
+    nb_total_medicaments = medicament_collection.count_distinct_agg(field_name="id_medicament")
+    
+    # 2.2. Total des pertes dues aux médicaments invendus
+    pertes_medicaments = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_valeur_perte, title="Calcul des pertes dues aux médicaments invendus")
+    try:
+        total_pertes_medicaments = pertes_medicaments[0]["perte_totale"] if pertes_medicaments else 0
+    except Exception as e:
+        st.error(f"❌ Erreur lors du calcul des pertes dues aux médicaments invendus : {e}")
+        total_pertes_medicaments = 0
+
+    # 2.4. Nombre total de fournisseur
+    nb_total_fournisseurs = medicament_collection.count_distinct_agg(field_name="fournisseur")
+
+    
+    # 2.5. Médicaments expirés ou bientôt expirés
+    medicament_collection = medicament_collection.get_collection()
+    # medicaments_expires = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_medicaments_expirants, title="Récupération des médicaments expirés ou bientôt expirés")
+    medicaments_expires = list(medicament_collection.aggregate(mongodb_pipelines.pipeline_expirations))
+
+
     medicament = df["medicament"]
     stock = df["stock"]
     vente_detail = df["detailVente"]
@@ -153,7 +212,6 @@ if df is not None and "medicament" in df and "stock" in df and "detailVente" in 
 
     try:
         # Statistiques générales
-        nb_total_medicaments = con.execute("SELECT COUNT(DISTINCT ID_Medicament) FROM medicaments").fetchone()[0]
 
         stats_stock = con.execute("""
             SELECT 
@@ -210,7 +268,7 @@ if df is not None and "medicament" in df and "stock" in df and "detailVente" in 
         # AFFICHAGE DESIGN
         with st.container():
 
-            col1,col2,col3,col4 = st.columns(4)
+            col1,col2,col4 = st.columns(3)
             col1.markdown(f"""
                 <div class="metric-box">
                     <div class="metric-label">🔢 Total Médicaments</div>
@@ -222,21 +280,21 @@ if df is not None and "medicament" in df and "stock" in df and "detailVente" in 
             col2.markdown(f"""
                 <div class="metric-box">
                     <div class="metric-label">📈 Total des pertes dues aux médicaments invendus</div>
-                    <div class="metric-value">{stats_stock["stock_max"][0]}</div>
+                    <div class="metric-value">{f"{total_pertes_medicaments:,}".replace(",", " ")} &nbsp;MGA</div>
                 </div>
             """, unsafe_allow_html=True)
 
-            col3.markdown(f"""
-                <div class="metric-box">
-                    <div class="metric-label">📊 Quantité totale de médicaments approvisionnés</div>
-                    <div class="metric-value">{stats_stock["stock_moyen"][0]}</div>
-                </div>
-            """, unsafe_allow_html=True)
+            # col3.markdown(f"""
+            #     <div class="metric-box">
+            #         <div class="metric-label">📊 Quantité totale de médicaments approvisionnés</div>
+            #         <div class="metric-value">{stats_stock["stock_moyen"][0]}</div>
+            #     </div>
+            # """, unsafe_allow_html=True)
 
             col4.markdown(f"""
                 <div class="metric-box">
                     <div class="metric-label">📊 Nombre total de fournisseurs</div>
-                    <div class="metric-value">{stats_stock["stock_moyen"][0]}</div>
+                    <div class="metric-value">{nb_total_fournisseurs}</div>
                 </div>
             """, unsafe_allow_html=True)
         
@@ -410,6 +468,7 @@ st.markdown(html_table, unsafe_allow_html=True)
 #         st.error(f"❌ Erreur lors du calcul des statistiques : {e}")
 # else:
 #     st.error("❌ Les données 'medicament', 'stock' et 'detailVente' ne sont pas présentes dans le DataFrame.")
+
 
 
 
