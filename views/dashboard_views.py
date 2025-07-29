079e38e0773437abb2b1b9531eea9fdfdc9f5bb2
promes27 <franchescopromes@gmail.com>
2025-07-29 13:47:27 +0300

    update dashbord

--- views/dashboard_views.py
+++ views/dashboard_views.py
@@ -8,25 +8,28 @@ from pipelines import pipeline_overview
 vente_collection = MongoDBClient(collection_name="vente")
 medicament_collection = MongoDBClient(collection_name="medicament")
 employe_collection = MongoDBClient(collection_name="employe")
+overview_collection = MongoDBClient(collection_name="overview")
+
 
 # 1. chiffre d'affaire total
-total_chiffre_affaire_str = f"{pipeline_overview.chiffe_affaire_total:,}".replace(",", " ")
-# chiffre_affaire = vente_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_chiffre_affaire, title="Calcul du chiffre d'affaire")
-# try:
-#     total_chiffre_affaire = chiffre_affaire[0]["montant_total"] if chiffre_affaire else 0
-# except Exception as e:
-#     total_chiffre_affaire = 0
+chiffre_affaire = overview_collection.make_specific_pipeline(pipeline=pipeline_overview.pipeline_chiffre_affaire, title="Calcul du chiffre d'affaire")
+try:
+  total_chiffre_affaire = chiffre_affaire[0]["chiffre_affaire_total"] if chiffre_affaire else 0
+  total_chiffre_affaire_str = f"{total_chiffre_affaire:,}".replace(",", " ")
+except Exception as e:
+  total_chiffre_affaire = 0
 
 # # 2. valeur totale du stock
-# valeur_stock = vente_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_somme_valeur_stock, title="Calcul de la valeur totale du stock")
-# try:
-#     valeur_totale_stock = valeur_stock[0]["valeur_totale_stock"] if valeur_stock else 0
-# except Exception as e:
-#     valeur_totale_stock = 0
+valeur_stock = overview_collection.make_specific_pipeline(pipeline=pipeline_overview.pipeline_valeur_totale_stock, title="Calcul de la valeur totale du stock")
+try:
+  valeur_totale_stock = valeur_stock[0]["valeur_stock_totale"] if valeur_stock else 0
+  valeur_totale_stock_str = f"{valeur_totale_stock:,}".replace(",", " ")
+except Exception as e:
+  valeur_totale_stock = 0
     
 # 3. nombre total de vente
-nombre_total_vente = vente_collection.count_distinct_agg(field_name="id_vente")
-nombre_total_vente_str = f"{pipeline_overview.total_sales:,}".replace(",", " ")
+nombre_total_vente = overview_collection.count_distinct_agg(field_name="id_vente")
+nombre_total_vente_str = f"{nombre_total_vente:,}".replace(",", " ")
 
 # 4. nombre total d'alimentation
 # nombre_alimentation = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_nombre_alimentations, title="Recuperation de nombre total d'alimentation")
@@ -35,24 +38,24 @@ nombre_total_vente_str = f"{pipeline_overview.total_sales:,}".replace(",", " ")
 # except Exception as e :
 #     nombre_total_alimentation = 0
 
-# II- SECOND LINE OF SCORECARD
- # 2.1. Nombre total de médicaments
-nb_total_medicaments = medicament_collection.count_distinct_agg(field_name="id_medicament")
+# # II- SECOND LINE OF SCORECARD
+#  # 2.1. Nombre total de médicaments
+# nb_total_medicaments = medicament_collection.count_distinct_agg(field_name="id_medicament")
     
-# 2.2. Total des pertes dues aux médicaments invendus
-total_pertes_medicaments = pipeline_overview.expired_medicines
-# pertes_medicaments = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_valeur_perte, title="Calcul des pertes dues aux médicaments invendus")
-# try:
-# except Exception as e:
-#   st.error(f"❌ Erreur lors du calcul des pertes dues aux médicaments invendus : {e}")
-#   total_pertes_medicaments = 0
+# # 2.2. Total des pertes dues aux médicaments invendus
+# total_pertes_medicaments = pipeline_overview.expired_medicines
+# # pertes_medicaments = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_valeur_perte, title="Calcul des pertes dues aux médicaments invendus")
+# # try:
+# # except Exception as e:
+# #   st.error(f"❌ Erreur lors du calcul des pertes dues aux médicaments invendus : {e}")
+# #   total_pertes_medicaments = 0
 
-# 2.4. Nombre total de fournisseur
-nb_total_fournisseurs = medicament_collection.count_distinct_agg(field_name="fournisseur")
+# # 2.4. Nombre total de fournisseur
+# nb_total_fournisseurs = medicament_collection.count_distinct_agg(field_name="fournisseur")
 
     
-# 2.5. Médicaments expirés ou bientôt expirés
-medicaments_expires = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_expirations, title="Récupération des médicaments expirés ou bientôt expirés")
+# # 2.5. Médicaments expirés ou bientôt expirés
+# medicaments_expires = medicament_collection.make_specific_pipeline(pipeline=mongodb_pipelines.pipeline_expirations, title="Récupération des médicaments expirés ou bientôt expirés")
 
 
 # STYLES
