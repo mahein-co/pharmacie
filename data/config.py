594877db003100f8fc3e2d3cdbcc321b915d7360
mahein <contact@mahein.co>
2025-08-15 09:51:43 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -16,17 +16,16 @@ mongo_cluster = os.getenv("MONGO_CLUSTER")
 hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-# if st.secrets:
-    # mongo_project_id = st.secrets["MONGO_PROJECT_ID"]
-    # mongo_public_key = st.secrets["MONGO_PUBLIC_KEY"]
-    # mongo_private_key = st.secrets["MONGO_PRIVATE_KEY"]
-
-    # mongo_app_name = st.secrets["MONGO_APP_NAME"]
-    # mongo_username = st.secrets["MONGO_USERNAME"]
-    # mongo_password = st.secrets["MONGO_PASSWORD"]
-    # mongo_cluster = st.secrets["MONGO_CLUSTER"]
-
-    # hf_token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
-    # openai_api_key = os.getenv("OPENAI_API_KEY")
+if st.secrets:
+    mongo_project_id = os.getenv("MONGO_PROJECT_ID")
+    mongo_public_key = os.getenv("MONGO_PUBLIC_KEY")
+    mongo_private_key = os.getenv("MONGO_PRIVATE_KEY")
     
+    mongo_app_name = os.getenv("MONGO_APP_NAME")
+    mongo_username = os.getenv("MONGO_USERNAME")
+    mongo_password = os.getenv("MONGO_PASSWORD")
+    mongo_cluster = os.getenv("MONGO_CLUSTER")
+    
+    hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    openai_api_key = os.getenv("OPENAI_API_KEY")  
 
