36b8ff2939c30f8351b309b87deaf5bed35dcf2f
BenitoFezay <benitofezay@gmail.com>
2025-08-18 13:49:20 +0300

    Recuperation du chiffre d'affaires.

--- views/dashboard_views.py
+++ views/dashboard_views.py
@@ -45,10 +45,10 @@ medicament_docs = medicament_collection.find_all_documents()
 # 1.1. chiffre d'affaire total
 chiffre_affaire = overview_collection.make_specific_pipeline(pipeline=pipeline_overview.pipeline_chiffre_affaire_total, title="Calcul du chiffre d'affaire")
 df_CA = pd.DataFrame(chiffre_affaire)
-print("valiny: ",df_CA.columns)
+print("valiny: ",df_CA["chiffre_affaire_total"].sum())
 try:
   total_chiffre_affaire = chiffre_affaire[0]["chiffre_affaire_total"] if chiffre_affaire else 0
-  total_chiffre_affaire_str = f"{int(total_chiffre_affaire):,}".replace(",", " ")
+  total_chiffre_affaire_str = f"{int(df_CA['chiffre_affaire_total'].sum()):,}".replace(",", " ")
 except Exception as e:
     total_chiffre_affaire_str = 0
 
